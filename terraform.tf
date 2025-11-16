4da59f7dfe96328dd9c2a2d22f2cb501a2c9bf08
mr-mayu-mane <mayurmane2577@gmail.com>
2025-11-16 16:15:38 +0530

    Add the backend for terraform state file

--- terraform.tf
+++ terraform.tf
@@ -5,7 +5,7 @@ terraform {
       version = "~> 6.0"
     }
   }
-}
+
 
 # Configure the AWS Provider
 provider "aws" {
@@ -15,4 +15,13 @@ provider "aws" {
 # Create a VPC
 resource "aws_vpc" "example" {
   cidr_block = "10.0.0.0/16"
+}
+
+backend "s3" {
+    bucket         = "Terraform State bucket"
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-state-table" 
+  }
+
 }
