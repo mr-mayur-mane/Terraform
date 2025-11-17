28cd53fdb4f033c9c3153060fbcb138b09f28da8
mr-mayu-mane <mayurmane2577@gmail.com>
2025-11-17 21:42:56 +0530

    Get the private ips from output

--- outputs.tf
+++ outputs.tf
@@ -1,13 +1,19 @@
 output "ec2_public_ip"{
     #value = aws_instance.my_instance[*].public_ip
     value = [
-        for instance in aws_instance.my_instance : key.public_ip
+        for instance in aws_instance.my_instance : instance.public_ip
     ]
 }
 
 output "ec2_public_dns"{
     #value = aws_instance.my_instance[*].public_dns
     value = [
-        for instance in aws_instance.my_instance : key.public_dns
+        for instance in aws_instance.my_instance : instance.public_dns
+    ]
+}
+
+output "ec2_public_ip"{
+    value = [
+        for instance in aws_instance.my_instance : instance.private_ip
     ]
 }
