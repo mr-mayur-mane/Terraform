9b5d1b63e38575f777c936605349328b037076ec
mr-mayu-mane <mayurmane2577@gmail.com>
2025-11-17 21:44:32 +0530

    Added the nginx file content

--- install_nginx.sh
+++ install_nginx.sh
@@ -5,4 +5,5 @@ apt install nginx -y
 systemctl enable nginx
 systemctl start nginx
 
-echo "<h1> This is my nginx page </h1>" > /var/www/html/index.html
+echo "<h1> This is my nginx home page </h1>" > /var/www/html/index.html
+echo "<h2> This is nginx hosting page </h2>" > /var/www/html/index.html"
