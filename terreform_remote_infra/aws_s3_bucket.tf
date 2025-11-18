2366557fe06866b7d7deb5e8726b109feef16845
mr-mayu-mane <mayurmane2577@gmail.com>
2025-11-18 21:15:02 +0530

    Fix the tags

--- terreform_remote_infra/aws_s3_bucket.tf
+++ terreform_remote_infra/aws_s3_bucket.tf
@@ -1,7 +1,8 @@
 resource aws_s3_bucket terraform_state{
     bucket = Terraform State bucket
 
-    tag = {
-        Name: "Terraform State bucket"
+    tags = {
+        Name = "Terraform State bucket"
+        Environment = "Prod"
     }
 }
