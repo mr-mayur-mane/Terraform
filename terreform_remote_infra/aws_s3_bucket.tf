7356c2667b4e70648d23bafa3a55424b4658d1e1
mr-mayu-mane <mayurmane2577@gmail.com>
2025-12-03 18:35:30 +0530

    Create the dev and prod s3 bucket for state file

--- terreform_remote_infra/aws_s3_bucket.tf
+++ terreform_remote_infra/aws_s3_bucket.tf
@@ -1,8 +1,16 @@
 resource aws_s3_bucket terraform_state{
-    bucket = Terraform State bucket
+    for_each = tomap({
+        my_bucket_prod = "Terraform State bucket Prod",
+        my_bucket_dev = "Terraform State bucket Dev"
+    })
+    bucket = each.value
 
     tags = {
-        Name = "Terraform State bucket"
+        for_each = tomap({
+            my_bucket_prod = "Production terraform State bucket",
+            my_bucket_dev = "Development terraform state bucket"
+        })
+        Name = each.value
         Environment = "Prod"
     }
 }
