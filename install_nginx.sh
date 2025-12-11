3bdb08fb4ab586ebc20de6fe09b9cdd35fcaac8b
mr-mayu-mane <mayurmane2577@gmail.com>
2025-12-11 21:49:49 +0530

    Added tags

--- install_nginx.sh
+++ install_nginx.sh
@@ -5,7 +5,9 @@ apt update -y
 apt install nginx -y
 
 systemctl enable nginx
+
 systemctl start nginx
 
 echo "<h1> This is my nginx home page </h1>" > /var/www/html/index.html
-echo "<h2> This is nginx hosting page </h2>" > /var/www/html/index.html"
+
+echo "<h2> This is nginx hosting page </h2>" > /var/www/html/index.html
