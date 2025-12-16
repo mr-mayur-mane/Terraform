af3522b8367980cd82f724208c99e939351cc9c0
mr-mayu-mane <mayurmane2577@gmail.com>
2025-12-16 19:00:04 +0530

    Recoorect the string format name/description

--- aws_ec2_instance.tf
+++ aws_ec2_instance.tf
@@ -1,12 +1,13 @@
 resource "aws_key_pair" "ssh_key"{
-  key_name  = my_ssh_key
+  key_name   = "my_ssh_key"
   public_key = file("pub_key")
 }
 
 resource "aws_default_vpc" "default_vpc"{
-  
+
 }
 resource "aws_security_group" "security_group"{
-  name  = allow all
-  description = Allow all incoming and outgoing traffic
+  name        = "allow all"
+  description = "Allow all incoming and outgoing traffic"
 }
+
