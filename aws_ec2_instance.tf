1471c78740cdf8f31e626266f0c5a078c0d0262b
mr-mayu-mane <mayurmane2577@gmail.com>
2025-12-16 19:05:20 +0530

    Added the cidr_blocks in security group

--- aws_ec2_instance.tf
+++ aws_ec2_instance.tf
@@ -9,5 +9,24 @@ resource "aws_default_vpc" "default_vpc"{
 resource "aws_security_group" "security_group"{
   name        = "allow all"
   description = "Allow all incoming and outgoing traffic"
+  vpn_id      = aws_default_vpc.default_vpc.id
+
+  ingress{
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_block  = ["0.0.0.0/0"]
+
+  }
+  egress{
+    from_port  = 0
+    to_port    = 0
+    protocol   = "-1"
+    cidr_block = ["0.0.0.0/0"]
+
+  }
+  tags={
+    Name  = "Allow all"
+  }
 }
 
