c5388ed93c468cf0df40590e3720e66bb6cc9da8
mr-mayu-mane <mayurmane2577@gmail.com>
2025-12-17 19:05:19 +0530

    Added the security group

--- terreform_remote_infra/test_aws.tf
+++ terreform_remote_infra/test_aws.tf
@@ -2,3 +2,13 @@ resource "aws_key_pair" "ssh_key_pair"{
     key_name    = "My SSH Key"
     file        = file("ssh_key")
 }
+
+resource "aws_default_vpc" "default_vpc"{
+
+}
+
+resource "aws_security_group" "my_security_group"{
+    name        = "Allow all"
+    description = "Allow all traffic"
+    vpc_id      = aws_default_vpc.default_vpc.id
+}
