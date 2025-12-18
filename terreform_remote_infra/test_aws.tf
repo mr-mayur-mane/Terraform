8ffa391ed71dba6233177a6eb406b97900b52cbb
mr-mayu-mane <mayurmane2577@gmail.com>
2025-12-18 19:18:31 +0530

    Corrected the syntx

--- terreform_remote_infra/test_aws.tf
+++ terreform_remote_infra/test_aws.tf
@@ -13,15 +13,15 @@ resource "aws_security_group" "my_security_group"{
     vpc_id  = aws_default_vpc.default.id
     
     ingress{
-        from_port   = "0"
-        to_port     = "0"
+        from_port   = 0
+        to_port     = 0
         protocol    = "-1"
         cidr_blocks = ["0.0.0.0/0"]
     }
 
     egress{
-        from_port   = "0"
-        to_port     = "0"
+        from_port   = 0
+        to_port     = 0
         protocol    = "-1"
         cidr_blocks = ["0.0.0.0/0"]
     }
