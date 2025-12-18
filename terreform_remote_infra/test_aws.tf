d9e6933b65dc2c1f57b6afc66831645533f43a84
mr-mayu-mane <mayurmane2577@gmail.com>
2025-12-18 19:15:40 +0530

    Corrected the syntx

--- terreform_remote_infra/test_aws.tf
+++ terreform_remote_infra/test_aws.tf
@@ -31,9 +31,9 @@ resource "aws_security_group" "my_security_group"{
     }
 }
 
-resource "aws_intance" "my_intance"{
+resource "aws_instance" "my_instance"{
     key_name                = aws_key_pair.my_ssh_key.key_name
-    vpc_secuirty_group_ids  = ["aws_security_group.my_security_group.id"]
+    vpc_security_group_ids  = ["aws_security_group.my_security_group.id"]
     ami                     = var.ec2_ami_id
     instance_type           = "t2.medium"
 
