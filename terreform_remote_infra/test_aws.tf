36314ac144fa19ef0b06f46a2e3332aafeb3e024
mr-mayu-mane <mayurmane2577@gmail.com>
2025-12-18 19:17:31 +0530

    Corrected the syntx

--- terreform_remote_infra/test_aws.tf
+++ terreform_remote_infra/test_aws.tf
@@ -23,7 +23,7 @@ resource "aws_security_group" "my_security_group"{
         from_port   = "0"
         to_port     = "0"
         protocol    = "-1"
-        cidr_blocks = ["0.0.0.0/0]
+        cidr_blocks = ["0.0.0.0/0"]
     }
     tags={
         Name = "Allow allow"
