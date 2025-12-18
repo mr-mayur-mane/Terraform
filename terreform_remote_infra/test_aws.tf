94c59b47e433368bb4779771435ca642249ae74f
mr-mayu-mane <mayurmane2577@gmail.com>
2025-12-18 19:08:13 +0530

    created secuirty group

--- terreform_remote_infra/test_aws.tf
+++ terreform_remote_infra/test_aws.tf
@@ -7,3 +7,9 @@ resource "aws_key_pair" "my_shh_key"{
 resource "aws_default_vpc" "default"{
 
 } 
+
+resource "aws_security_group" "my_security_group"{
+    name    = "Alloe all"
+    vpc_id  = aws_default_vpc.default.id
+    
+}
