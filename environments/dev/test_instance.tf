da9df49bbd2ceb8f941f7b8d76651ac89384fbf1
mr-mayu-mane <mayurmane2577@gmail.com>
2025-12-22 19:05:58 +0530

    create the security group with vpc id

--- environments/dev/test_instance.tf
+++ environments/dev/test_instance.tf
@@ -4,5 +4,10 @@ resource "aws_key_pair" "my_ssh"{
 }
 
 resource "aws_default_vpc" "default"{
-    
+
+}
+
+resource "aws_security_group" "my_security_group"{
+    name   = "my_security_group"
+    vpc_id = aws_default_vpc.default.id
 }
