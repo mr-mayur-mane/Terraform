e89b07ea2ebd2a7da46bd269a450edca0d23f90e
mr-mayu-mane <mayurmane2577@gmail.com>
2025-12-22 19:08:07 +0530

    Create the full secuirty group to allow all

--- environments/dev/test_instance.tf
+++ environments/dev/test_instance.tf
@@ -10,4 +10,23 @@ resource "aws_default_vpc" "default"{
 resource "aws_security_group" "my_security_group"{
     name   = "my_security_group"
     vpc_id = aws_default_vpc.default.id
+
+    ingress{
+        from_port = 0
+        to_port   = 0
+        protocol  = "-1"
+        cidr_blocks = ["0.0.0.0/0"] 
+    }
+
+    egress{
+        from_port = 0
+        to_port   = 0 
+        protocol  = -1
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    tags={
+        Name = "Allow all"
+        Description = "Allow allow traffic"
+    }
 }
