94973bbda8c4b194702acbabc221e2c5b500ad69
mr-mayu-mane <mayurmane2577@gmail.com>
2025-12-23 19:00:47 +0530

    Added the loop over the instance type

--- environments/dev/test_instance.tf
+++ environments/dev/test_instance.tf
@@ -32,9 +32,13 @@ resource "aws_security_group" "my_security_group"{
 }
 
 resource "aws_instance" "my_instance"{
+    for_each = tomap({
+        my_instance_1 = t2.micro,
+        my_instance_2 = t2.small
+    })
     key_name = aws_key_pair.my_ssh.key_name
     vpc_security_group_ids = [aws_security_group.my_security_group]
-    instance_key  = "t2.micro"
+    instance_key  = "each.value"
     ami = var.ec2_ami_id
 
 
