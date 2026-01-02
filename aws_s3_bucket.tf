6740f9c9200083855da3511d009d7a1a0464545e
mr-mayu-mane <mayurmane2577@gmail.com>
2026-01-02 17:34:32 +0530

    Created the s3 bucket for dev env

--- aws_s3_bucket.tf
+++ aws_s3_bucket.tf
@@ -1,3 +1,8 @@
 resource "aws_s3_bucket" "my_bucket"{
     bucket = "My S3 Bucket"
+    tags={
+        Name = "Development Bucket"
+        Description = "Development Bucket"
+        Environment = "DEV"
+    }
 }
