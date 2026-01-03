1b05ec5acbbd16806ba65af4541d37b2b12bb364
mr-mayu-mane <mayurmane2577@gmail.com>
2026-01-03 19:45:10 +0530

    Added the attributes and tages to dynamo_table

--- terreform_remote_infra/aws_dynamodb_table.tf
+++ terreform_remote_infra/aws_dynamodb_table.tf
@@ -18,4 +18,14 @@ resource "aws_dynamodb_table" "terraform_state_table_"{
     name         = "terraform_state_table"
     billing_mode = "PAY_PER_REQUEST"
     hash_key     =  "lock_id" 
+
+    attribute{
+      name = "lock_id"
+      type = "S"
+    }
+
+    tags={
+      Name = "Terraform state table"
+      Description = "Terreform state table to access the state file once per request"
+    }
 }
