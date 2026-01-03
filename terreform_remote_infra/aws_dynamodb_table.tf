b2ed021c7350f78d06cf3e374522da4916a21092
mr-mayu-mane <mayurmane2577@gmail.com>
2026-01-03 19:47:00 +0530

    Cleared the unwanted dynamodb_table content

--- terreform_remote_infra/aws_dynamodb_table.tf
+++ terreform_remote_infra/aws_dynamodb_table.tf
@@ -1,19 +1,3 @@
-resource "aws_dynamodb_table" "terraform_table" {
-  name           = "terraform-state-table"
-  billing_mode   = "PAY_PER_REQUEST"
-  hash_key       = "LockID"
-
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-
-
-  tags = {
-    Name   = "terraform-state-table"
-  }
-}
-
 resource "aws_dynamodb_table" "terraform_state_table_"{
     name         = "terraform_state_table"
     billing_mode = "PAY_PER_REQUEST"
