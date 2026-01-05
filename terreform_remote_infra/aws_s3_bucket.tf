2a179c3a6e7303fc1e8092f3068aaa8cdd8b5fad
mr-mayu-mane <mayurmane2577@gmail.com>
2026-01-05 19:25:47 +0530

    Added the tags

--- terreform_remote_infra/aws_s3_bucket.tf
+++ terreform_remote_infra/aws_s3_bucket.tf
@@ -2,11 +2,13 @@ resource "aws_s3_bucket" "terraform_state"{
     for_each = {
         prod={
             bucket_name = "Terraform dev bucket"
+
             environment = "dev"
         }
 
         dev = {
             bucket_name = "Terraform prod bucket"
+            
             environment = "prod"
         }
     }
@@ -15,7 +17,7 @@ resource "aws_s3_bucket" "terraform_state"{
 
     tags={
         Name = each.value.bucket_name
-        
+
         Environment = each.value.environment
     }
 }
