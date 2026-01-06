e7450af9c3d27fec5848be7c3ae9fe97339eb8b4
mr-mayu-mane <mayurmane2577@gmail.com>
2026-01-06 18:39:55 +0530

    Add the dynamic value for he tags

--- aws_s3_bucket.tf
+++ aws_s3_bucket.tf
@@ -1,18 +1,18 @@
 resource "aws_s3_bucket" "my_bucket"{
     for_each= tomap({
         prod = {
-            bucket_name = "Terraform Prod"
+            bucket_name = "Terraform Prod",
             environment = "prod" 
         }
 
         dev= {
-            bucket_name = "Terraform Dev"
+            bucket_name = "Terraform Dev",
             environment = "dev"
         }
     })
     bucket = each.value.bucket_name
     tags={
-        Name = "Development Bucket"
+        Name = each.value.bucket_name
         Description = "Development Bucket"
         Environment = each.value.environment
     }
