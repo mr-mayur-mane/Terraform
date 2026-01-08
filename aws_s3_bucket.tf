a666079478ba0243a1d118a35870d5a252fadb9d
mr-mayu-mane <mayurmane2577@gmail.com>
2026-01-08 21:50:56 +0530

    Added the description in the tags

--- aws_s3_bucket.tf
+++ aws_s3_bucket.tf
@@ -3,17 +3,18 @@ resource "aws_s3_bucket" "my_bucket"{
         prod = {
             bucket_name = "Terraform Prod"
             environment = "prod" 
+            description = "This is terraform prod bucket"
         }
-
         dev= {
             bucket_name = "Terraform Dev"
             environment = "dev"
+            description = "This is terraform dev bucket"
         }
     }
     bucket = each.value.bucket_name
     tags={
         Name = each.value.bucket_name
-        Description = each.value.bucket_name
+        Description = each.value.description
         Environment = each.value.environment
     }
 }
