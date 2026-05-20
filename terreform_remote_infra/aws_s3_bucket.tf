b6e4134f9afc904ac2e750f8147b56c084ebd15e
mr-mayu-mane <mayurmane2577@gmail.com>
2026-05-20 18:38:47 +0530

    Create the test bucket

--- terreform_remote_infra/aws_s3_bucket.tf
+++ terreform_remote_infra/aws_s3_bucket.tf
@@ -1,26 +1,31 @@
-resource "aws_s3_bucket" "terraform_state"{
-    for_each = {
-        prod = {
-            bucket_name = "Terraform dev bucket"
-            environment = "dev"
-        }
+# resource "aws_s3_bucket" "terraform_state"{
+#     for_each = {
+#         prod = {
+#             bucket_name = "Terraform dev bucket"
+#             environment = "dev"
+#         }
 
-        dev = {
-            bucket_name = "Terraform prod bucket"           
-            environment = "prod"
-        }
-    }
+#         dev = {
+#             bucket_name = "Terraform prod bucket"           
+#             environment = "prod"
+#         }
+#     }
 
-    bucket = each.value.bucket_name
+#     bucket = each.value.bucket_name
 
-    tags={
-        Name = each.value.bucket_name
+#     tags={
+#         Name = each.value.bucket_name
 
-        Environment = each.value.environment
-    }
-}
+#         Environment = each.value.environment
+#     }
+# }
 
 resource "aws_s3_bucket" "my-bucket"{
     bucket= "my-test-bucket"
+
+    tags={
+        Name = "test"
+        Description = "this is the test bucket"
+    }
 
 }
