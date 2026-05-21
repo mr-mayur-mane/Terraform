32ad592709e58dc03a31d22bd62caab13a9d818c
mr-mayu-mane <mayurmane2577@gmail.com>
2026-05-21 18:00:33 +0530

    Fixed the priovider

--- terraform.tf
+++ terraform.tf
@@ -6,26 +6,10 @@ terraform {
     }
   }
 
-
-# Configure the AWS Provider
-provider "aws" {
-  region = "us-east-1"
-}
-
-# Create a VPC
-
-resource "aws_vpc" "example" {
-  
-  cidr_block = "10.0.0.0/16"
-}
-
-#add the backend
-
 backend "s3" {
     bucket         = "Terraform State bucket"
     key            = "terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-state-table" 
   }
-
 }
